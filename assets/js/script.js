cc674b51b08ad4c3256218435ec3f2d1f478ecd1
Carolyn B <bmathematics@gmail.com>
2022-01-19 13:49:22 -0500

    changed listening event to submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEL = document.querySelector("#tasks-to-do");
 
 
 
 var createTaskHandler = function(){
+
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEL.appendChild(listItemEl);
 };
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
